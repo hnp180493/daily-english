f23d48f01a90d2104888f0e42aa041194797540e
hnp180493 <hnp180493@gmail.com>
2025-11-10 23:01:04 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   aiProvider: 'azure' as 'azure' | 'gemini' | 'openai',
   supabase: {
-    url: 'https://pxxbalgsvnpvxhnnkeyg.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4eGJhbGdzdm5wdnhobm5rZXlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1OTY3MjAsImV4cCI6MjA3ODE3MjcyMH0.ymh1h-2JR3Hl1D9vB_cKHsHBxQU4Rxd5XWNZbTLaHks'
+    url: '',
+    anonKey: ''
   },
   seo: {
     googleSiteVerification: '7JzL97lfcCS6JB8NHzOdxfhO20y2H8tkScCfogsuMbY', // Add your Google Search Console verification code here
